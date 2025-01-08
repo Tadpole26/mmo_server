05387530eccb6b3756f8fcf987be5421c94ea157
zz <844874696@qq.com>
2025-01-08 17:35:29 +0800

    add recastnavigation lib

---
+++ Libs/3rdlibs/recastnavigation/mybuild/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: D:/xudunke/navigation/recastnavigation-main/recastnavigation
+# Build directory: D:/xudunke/navigation/recastnavigation-main/recastnavigation/mybuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("DebugUtils")
+subdirs("Detour")
+subdirs("DetourCrowd")
+subdirs("DetourTileCache")
+subdirs("Recast")
+subdirs("Tests")
